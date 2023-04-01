066c27abdc623492f5b4acf01a653896cb964979
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-04-01 15:21:51 -0700

    worked on project

---
+++ memory-card-project/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./App.css";
+import Game from "./components/Game";
+import Header from "./components/Header";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentScore: 0,
+      bestScore: 0,
+    };
+    this.handleScore = this.handleScore.bind(this);
+  }
+  handleScore(increment) {
+    if (increment) {
+      this.setState({
+        currentScore: this.state.currentScore + 1,
+      });
+    } else {
+      this.setState({
+        currentScore: 0,
+      });
+    }
+  }
+  render() {
+    const { currentScore, bestScore } = this.state;
+    return (
+      <div className="App">
+        <Header currentScore={currentScore} bestScore={bestScore} />
+        <Game handleScore={this.handleScore} />
+      </div>
+    );
+  }
+}
+
+export default App;
